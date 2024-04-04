3a929b53518829cc12f0605fdeefcd0cedf725b6
aeshak24 <aeshak@udel.edu>
2024-04-04 16:49:38 -0400

    added lilypads and connected the lines

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -1,9 +1,12 @@
+import { Linter } from "eslint";
 import Phaser from "phaser";
 
 export default class Level1 extends Phaser.Scene {
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
     private kid?: Phaser.Physics.Arcade.StaticGroup;
     private mom?: Phaser.Physics.Arcade.StaticGroup;
+    private lilypads?: Phaser.Physics.Arcade.StaticGroup;
+    private line1?: Phaser.Geom.Line;
 
     // private score: number = 0;
     // private scoreText?: Phaser.GameObjects.Text;
@@ -23,6 +26,38 @@ export default class Level1 extends Phaser.Scene {
         image.setScale(scale).setScrollFactor(0);
         let kid = this.add.image(180, 550, "froghappy");
         let mom = this.add.image(1000, 150, "froghappy");
+
+        const graphics = this.add.graphics();
+        graphics.lineStyle(2, 0x0000ea);
+
+        //  ST
+        graphics.beginPath();
+        graphics.moveTo(600, 568);
+        graphics.lineTo(800, 300);
+        graphics.lineTo(400, 300);
+        graphics.lineTo(280, 150);
+        graphics.lineTo(600, 150);
+        graphics.closePath();
+        graphics.strokePath();
+
+        //const line = new Phaser.Geom.Line(200, 300, 700, 100);
+
+        this.lilypads = this.physics.add.staticGroup();
+        const pads = this.lilypads.create(
+            600,
+            568,
+            "lilypad"
+        ) as Phaser.Physics.Arcade.Sprite;
+        pads.setScale(0.5).refreshBody();
+
+        this.lilypads.create(800, 300, "lilypad").setScale(0.5).refreshBody();
+        this.lilypads.create(280, 150, "lilypad").setScale(0.5).refreshBody();
+        this.lilypads.create(400, 300, "lilypad").setScale(0.5).refreshBody();
+        this.lilypads.create(600, 150, "lilypad").setScale(0.5).refreshBody();
+        this.add.line(0, 0, 600, 568, 800, 300, 0x006400, 1);
+
+        this.add.line(50, 50, 100, 100, 200, 200, 0xff0000);
+
         mom.setScale(2);
         kid.setScale(1);
         const message = `Level 1`;
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -8,6 +8,7 @@ export default class PreloadScene extends Phaser.Scene {
     preload() {
         this.load.image("frogBackground", "assets/bg.png");
         this.load.image("froghappy", "assets/faces/glad.png");
+        this.load.image("lilypad", "assets/lilypad.png");
     }
 
     create() {
