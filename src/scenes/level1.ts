28bf3de4bbf3c0a15d6cf206f02a714f5bd6730c
Kristina <kbalta@udel.edu>
2024-04-06 15:34:00 -0400

    changed lilpyads to different sprites, added numbers to each lilypad node, added path length to screen

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -5,11 +5,15 @@ export default class Level1 extends Phaser.Scene {
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
     private kid?: Phaser.Physics.Arcade.StaticGroup;
     private mom?: Phaser.Physics.Arcade.StaticGroup;
-    private lilypads?: Phaser.Physics.Arcade.StaticGroup;
-    private line1?: Phaser.Geom.Line;
+    private l1?: Phaser.GameObjects.Sprite;
+    private l2?: Phaser.GameObjects.Sprite;
+    private l3?: Phaser.GameObjects.Sprite;
+    private l4?: Phaser.GameObjects.Sprite;
+    private l5?: Phaser.GameObjects.Sprite;
+    private l6?: Phaser.GameObjects.Sprite;
 
-    // private score: number = 0;
-    // private scoreText?: Phaser.GameObjects.Text;
+    private score: number = 0;
+    private scoreText?: Phaser.GameObjects.Text;
     constructor() {
         super({ key: "Level1" });
     }
@@ -24,11 +28,9 @@ export default class Level1 extends Phaser.Scene {
         let scaleY = this.cameras.main.height / image.height;
         let scale = Math.max(scaleX, scaleY);
         image.setScale(scale).setScrollFactor(0);
-        let kid = this.add.image(235, 415, "froghappy");
-        let mom = this.add.image(1000, 150, "froghappy");
 
         const graphics = this.add.graphics();
-        graphics.lineStyle(2, 0x0000ea);
+        graphics.lineStyle(2, 0x000000);
 
         //  PATH
         graphics.beginPath();
@@ -52,20 +54,39 @@ export default class Level1 extends Phaser.Scene {
         graphics.lineTo(900, 500);
         graphics.strokePath();
 
-        this.lilypads = this.physics.add.staticGroup();
-        const pads = this.lilypads.create(
-            600,
-            450,
-            "lilypad"
-        ) as Phaser.Physics.Arcade.Sprite;
-        pads.setScale(0.5).refreshBody();
+        this.l1 = this.add
+            .sprite(600, 450, "lilypad")
+            .setScale(0.5)
+            .setAngle(150)
+            .setInteractive();
+        this.l2 = this.add
+            .sprite(800, 300, "lilypad")
+            .setScale(0.5)
+            .setAngle(100)
+            .setInteractive();
+        this.l3 = this.add
+            .sprite(280, 150, "lilypad")
+            .setScale(0.5)
+            .setAngle(45)
+            .setInteractive();
+        this.l4 = this.add
+            .sprite(400, 300, "lilypad")
+            .setScale(0.5)
+            .setAngle(280)
+            .setInteractive();
+        this.l5 = this.add
+            .sprite(600, 150, "lilypad")
+            .setScale(0.5)
+            .setAngle(0)
+            .setInteractive();
+        this.l6 = this.add
+            .sprite(900, 500, "lilypad")
+            .setScale(0.5)
+            .setAngle(230)
+            .setInteractive();
 
-        this.lilypads.create(800, 300, "lilypad").setScale(0.5).refreshBody();
-        this.lilypads.create(280, 150, "lilypad").setScale(0.5).refreshBody();
-        this.lilypads.create(400, 300, "lilypad").setScale(0.5).refreshBody();
-        this.lilypads.create(600, 150, "lilypad").setScale(0.5).refreshBody();
-        this.lilypads.create(900, 500, "lilypad").setScale(0.5).refreshBody();
-
+        let kid = this.add.image(235, 415, "froghappy");
+        let mom = this.add.image(1000, 150, "froghappy");
         mom.setScale(2);
         kid.setScale(1);
         const message = `Level 1`;
@@ -73,15 +94,60 @@ export default class Level1 extends Phaser.Scene {
             .text(this.cameras.main.width - 15, 15, message, {
                 color: "#000000",
                 fontSize: "40px",
+                fontStyle: "bold",
             })
             .setOrigin(1, 0);
 
+        this.add
+            .text(295, 130, "1", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
+        this.add
+            .text(615, 130, "2", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
+        this.add
+            .text(815, 280, "1", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
+
+        this.add
+            .text(415, 280, "4", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
+        this.add
+            .text(615, 430, "2", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
+        this.add
+            .text(915, 480, "3", {
+                color: "#ffffff",
+                fontSize: "35px",
+                fontStyle: "bold",
+            })
+            .setOrigin(1, 0);
         this.input.keyboard?.createCursorKeys();
         this.cursors = this.input.keyboard?.createCursorKeys();
-        // this.scoreText = this.add.text(16, 16, "Score: " + this.score, {
-        //     fontSize: "32px",
-        //     color: "#000",
-        // });
+        this.scoreText = this.add.text(16, 16, "Path Length: " + this.score, {
+            fontSize: "45px",
+            color: "#000",
+            fontStyle: "bold",
+        });
     }
 
     update() {}
