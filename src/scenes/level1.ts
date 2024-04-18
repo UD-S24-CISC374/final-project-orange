3001175f6ef06555e36e1b7b132ee8ed6324c84e
Kristina <kbalta@udel.edu>
2024-04-18 17:02:03 -0400

    splash

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -49,12 +49,14 @@ export default class Level1 extends Phaser.Scene {
         graphics.lineTo(650, 450);
         graphics.lineTo(235, 415);
         graphics.strokePath();
+        const splash = this.sound.add("splash2", { loop: false });
 
         let kid = this.add.image(235, 415, "froghappy");
         let mom = this.add.image(1000, 150, "froghappy");
         mom.setScale(2)
             .setInteractive()
             .on("pointerdown", () => {
+                splash.play();
                 if (kid.x == 500) {
                     this.score += 1;
                     kid.setX(mom.x + 10)
